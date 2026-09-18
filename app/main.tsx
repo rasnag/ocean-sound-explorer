149df5ecea3f4f7f505b0d374351571fb6f89e2e
rasnag <rasna88@gmail.com>
2026-09-17 22:09:51 -0400

    Route Curiosity Loop experiences

--- app/main.tsx
+++ app/main.tsx
@@ -2,6 +2,8 @@
 import {createRoot} from 'react-dom/client';
 import Home from './page';
 import MakeMoon from './MakeMoon';
+import About from './About';
 import './globals.css';
-const isMoon=window.location.pathname.replace(/\/$/,'')==='/make-the-moon';
-createRoot(document.getElementById('root')!).render(isMoon?<MakeMoon/>:<Home/>);
+const path=window.location.pathname.replace(/\/$/,'')||'/';
+const page=path==='/make-the-moon'?<MakeMoon/>:path==='/ocean-sound-explorer'?<Home/>:<About/>;
+createRoot(document.getElementById('root')!).render(page);
